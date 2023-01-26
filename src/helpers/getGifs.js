513ad1637b1cbe338870e505e066494c053e34b7
Joan <jdiaz31@udi.edu.co>
2023-01-26 15:58:41 -0500

    Primer commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,18 @@
+
+
+export const getGifs = async(category)=>{
+
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=eg6Q23jeWmWUrqmNNYg62R6YRV6TuzOD&q=${category}&limit=16`;
+    const resp = await fetch ( url );
+    const { data } = await resp.json();
+
+    const gifs = data.map( img => ({
+        id: img.id,
+        tittle: img.title,
+        url: img.images.downsized_medium.url
+        
+    }));
+
+    console.log(gifs);
+    return gifs;
+}
